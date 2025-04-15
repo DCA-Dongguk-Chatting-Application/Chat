e2e61d2ac5bc8cf552240b62dc681c4d5d561f29
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-04-15 12:30:42 +0900

    feat: 스프링 시큐리티, 스웨거 설정 파일 생성

---
+++ back/src/main/java/com/dongguk/chat/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.dongguk.chat.config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/api/v1/**").permitAll()
+                                .anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
---
+++ back/src/main/java/com/dongguk/chat/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.dongguk.chat.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api(){
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .addOpenApiCustomizer(openApi -> {
+                    openApi.getComponents().addParameters("Authorization", createHeader("Authorization"));
+                })
+                .build();
+    }
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info().title("MYS DATABASE TRANSACTION API DOCUMENT")
+                        .description("TODO 웹 애플리케이션의 데이터베이스 트랜잭션을 관리하는 API입니다.")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+    private Parameter createHeader(String name){
+        return new Parameter()
+                .in("header")
+                .schema(new io.swagger.v3.oas.models.media.StringSchema())
+                .name(name)
+                .description(name)
+                .required(false);
+    }
+}
