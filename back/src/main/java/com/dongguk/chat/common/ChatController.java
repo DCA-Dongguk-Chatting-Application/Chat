7bb228956ec739756533c8b47b6ef1d16651fa33
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-04-16 14:27:53 +0900

    feat: create simple chat example

---
+++ back/src/main/java/com/dongguk/chat/common/ChatController.java
@@ -0,0 +1,15 @@
+package com.dongguk.chat.common;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String handle(String message){
+        return message;
+    }
+}
